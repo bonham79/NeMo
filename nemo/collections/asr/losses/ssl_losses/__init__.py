706247aa9441aa6e46dcd5c96db9d987128384c7
Bonham79 <travismbartley@gmail.com>
2023-03-14 13:45:10 -0400

    forgot inits
    Signed-off-by: Bonham79 <travismbartley@gmail.com>

--- nemo/collections/asr/losses/ssl_losses/__init__.py
+++ nemo/collections/asr/losses/ssl_losses/__init__.py
@@ -12,4 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from nemo.collections.asr.losses.ssl_losses.contrastive import ContrastiveLoss
+from nemo.collections.asr.losses.ssl_losses.predictive import PredictiveLoss
+from nemo.collections.asr.losses.ssl_losses.contrastive import ContrastiveLoss, FixedCodebookContrastiveLoss
+from nemo.collections.asr.losses.ssl_losses.reconstructive import ReconstructiveLoss
